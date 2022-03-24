7b34d3632dfdb81bdbaa8ca0a6c0b1b9fcbc4ffe
Nina Tan <ninavtan@gmail.com>
2022-03-24 19:03:44 -0400

    added favicon, cursor, and responsiveness

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -8,27 +8,32 @@ import styled from 'styled-components'
 
 export default function FrontPage() {
   return (
+    <Container fluid>
       <Row className="front-page-row">
-        <Col xs={2} md={2}>
-        <LinkColumn>
-          <Link to="/reading-now">What I'm Reading Now</Link>
-          <Link to="/reading-now">Plant Updates</Link>
-        </LinkColumn>
+        <h4 className="headline">Software engineer passionate about inclusive tech.</h4>
+        <Col md={2} xl={1} xxl={1}>
+          <LinkColumn>
+            <Link to="/reading-now">What I'm Reading Now</Link>
+            <Link to="/reading-now">Plant Updates</Link>
+          </LinkColumn>
           
         </Col>
-        <Col xs={4} md={8}>
+        {/* <Col xs={12} sm={12} md={10}> */}
+        <Col md={2} xxl={10}>
           <div className="banner-container">
-          <img id="banner-image" alt="new-zealand" src="https://drive.google.com/uc?export=view&id=1rrxT9Ar6josuOssuj0QJIViPRqDn7CXF"></img>
+            <img id="banner-image" alt="new-zealand" src="https://drive.google.com/uc?export=view&id=1rrxT9Ar6josuOssuj0QJIViPRqDn7CXF"></img>
           </div>
         </Col>
-        <Col xs={2} md={2}>
-        <LinkColumn>
-          <a target="_blank" href="https://letterboxd.com/ninoqueen/">My Letterboxd</a>
-          <a target="_blank" href="https://letterboxd.com/ninoqueen/">Seasonal Plans</a>
+
+        <Col md={2} xl={1} xxl={1}>
+          <LinkColumn>
+            <a target="_blank" href="https://letterboxd.com/ninoqueen/">My Letterboxd</a>
+            <Link to="/reading-now">Sooo random</Link>
           </LinkColumn>
         </Col>
 
       </Row>
+      </Container>
       
     
   )
@@ -36,10 +41,12 @@ export default function FrontPage() {
 
 const LinkColumn = styled.div`
   height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
-  align-content: stretch;
+  align-content: space-between;
   justify-content: space-around;
+  text-align: center;
   
   
   
--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -24,12 +24,24 @@ export default function NavigationHeader() {
   <div className="App">
       
         <Row className="App-header">
-          <Col className="navigation">
+          <Col xxl={1} className="links-navigation">
+            <div className="links">
+              <a href="https://github.com/ninavtan" target="_blank" rel="noreferrer"><img alt="github-link" src="https://img.icons8.com/ios-glyphs/30/000000/github.png"/></a>
+              <a href="https://linkedin.com/in/ninatan8" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin.png"/></a>
+              
+            </div>
+          
+          </Col>
+          <Col xxl={10} className="navigation">
             <button type="button" className="navigation-links" onClick={handleBackClick}>Back</button>
               <img alt="nina" className="logo-nav" src="https://drive.google.com/uc?export=view&id=1692vnCagVIGK-94rS8d0rWfqmmpYf_GO"></img>
               
             <button type="button" onClick = {handleForwardClick} className="navigation-links"><i class="bi bi-caret-left"></i>Forward</button>
-
+          </Col>
+          <Col xxl={1} className="links-navigation">
+            <div className="links">
+              
+            </div>
           </Col>
 
       </Row>
